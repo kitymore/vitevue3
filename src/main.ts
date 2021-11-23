92fae4975330aae62b628103db34a5b5b9f1596d
kitymore <2560352426@qq.com>
2021-11-23 13:47:45 +0800

    fea/vue3架子初始化

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router/index'
+import store from './store/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+createApp(App).use(store).use(ElementPlus).use(router).mount('#app')
---
+++ src/store/index.ts
@@ -0,0 +1,27 @@
+import { createStore } from 'vuex'
+
+const defaultState = {
+  count: 0
+}
+
+// todo简单配置如下：后面可能会按模块切分
+export default createStore({
+  state() {
+    return defaultState
+  },
+  mutations: {
+    increment(state: typeof defaultState) {
+      state.count++
+    }
+  },
+  actions: {
+    increment(context) {
+      context.commit('increment')
+    }
+  },
+  getters: {
+    double(state: typeof defaultState) {
+      return 2 * state.count
+    }
+  }
+})
